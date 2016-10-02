54101039d97855096b3cfdaf7fc29817c7759ae1
Reynard Le <reynard.le@gmail.com>
2016-10-02 00:01:33 -0700

    Add recursive exercises

---
+++ recursive.rb
@@ -0,0 +1,19 @@
+# Exercise 1:
+# Write recursive function natural_reverse(n) which takes a natural number n 
+# and returns the natural number whose digits are the digits of n in reverse order.
+# Examples:
+# natural_reverse(12345) = 54321
+# natural_reverse(12300) = 321
+# Note: usage of temporary array is disallowed.
+
+
+
+# Exercise 2:
+# Write recursive function binary_to_decimal(binary_string) which takes a binary string
+# and returns the decimal number equivalent of that binary.
+# The binary string's most significant bit is the leftmost one.
+# Note: usage of substring is allowed but splitting string into array is not.
+# Examples:
+# binary_to_decimal("101") = 5
+# binary_to_decimal("1") = 1
+# binary_to_decimal("001001") = 9
